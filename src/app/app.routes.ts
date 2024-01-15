fd95a2a067d164d80989f321f91967a9451bcd7f
Jens Meichler <j.meichler@rebuy.com>
2024-01-15 16:41:51 +0100

    fix: cumulative layout shift

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import {Routes} from '@angular/router';
+import {HomeComponent} from "src/app/pages/home/home.component";
+import {ContactComponent} from "src/app/pages/contact/contact.component";
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadComponent: () =>
-      import('./pages/home/home.component')
-        .then(x => x.HomeComponent)
+    component: HomeComponent
   },
   {
     path: 'behandlungen',
@@ -28,8 +28,6 @@ export const routes: Routes = [
   },
   {
     path: 'kontakt',
-    loadComponent: () =>
-      import('./pages/contact/contact.component')
-        .then(x => x.ContactComponent)
+    component: ContactComponent
   },
 ];
