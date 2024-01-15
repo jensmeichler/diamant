4f3892cc532e1063f51a363c2ad66e737f731858
Jens Meichler <j.meichler@rebuy.com>
2024-01-15 20:01:42 +0100

    feat: read more btn for home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,18 @@
 import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {AsyncPipe, NgClass, NgIf} from "@angular/common";
+import {fromEvent, map, startWith} from "rxjs";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf, AsyncPipe, NgClass],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  $desktop = fromEvent(window, 'resize')
+    .pipe(startWith(undefined), map(() => window.innerWidth >= 650));
+
+  readMore = false;
 }
