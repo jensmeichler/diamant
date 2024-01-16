123c56060ced415a983c3858383b537146635a82
Jens Meichler <j.meichler@rebuy.com>
2024-01-16 11:11:25 +0100

    fix: use routerlink instead of href

--- src/app/pages/treatments/treatments.component.ts
+++ src/app/pages/treatments/treatments.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-treatments',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './treatments.component.html',
   styleUrls: ['./treatments.component.scss']
 })
