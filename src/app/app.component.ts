abfc05717047c107a32fc84122bfc5af13f117c9
Jens Meichler <j.meichler@rebuy.com>
2024-01-19 15:50:25 +0100

    refactor: pages structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "src/app/components/header/header.component";
+import {FooterComponent} from "src/app/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, RouterLink],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, RouterLink, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
