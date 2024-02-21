83bd2e81489be9117436f84cb13aa9f0e1fa25f9
Jens Meichler <j.meichler@rebuy.com>
2024-02-21 22:22:06 +0100

    feat: floating whatsapp button for mobile contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -4,16 +4,19 @@ import {SeoService} from "src/app/services/seo.service";
 import {ImgComponent} from "src/app/components/img/img.component";
 import {RatingComponent} from "src/app/components/rating/rating.component";
 import {TreatmentsExplanationsComponent} from "src/app/pages/treatments-explanations/treatments-explanations.component";
+import {AsyncPipe, NgIf} from "@angular/common";
+import {ViewService} from "src/app/services/view.service";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [MapComponent, ImgComponent, RatingComponent, TreatmentsExplanationsComponent],
+  imports: [MapComponent, ImgComponent, RatingComponent, TreatmentsExplanationsComponent, AsyncPipe, NgIf],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
   seo = inject(SeoService);
+  mobile$ = inject(ViewService).mobile$;
 
   ngOnInit(): void {
     window.scroll({top: 0, behavior: 'instant'});
