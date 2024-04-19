d33294ba82a5d5c4e6a2afaf3342fbca1dabeb01
Jens Meichler <j.meichler@rebuy.com>
2024-04-19 10:33:56 +0200

    feat: cookie consent for google maps iframe

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,11 +1,18 @@
 import {Component} from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent {
+  consentGiven = localStorage.getItem('consent') === 'true';
+
+  giveConsent(): void {
+    this.consentGiven = true;
+    localStorage.setItem('consent', 'true');
+  }
 }
