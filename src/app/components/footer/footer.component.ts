770a4bf3604a6b1e44fb0335ba3368a02e1c5305
Jens Meichler <j.meichler@rebuy.com>
2024-05-03 10:06:13 +0200

    feat(i18n): ngx-translate setup

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,11 +2,12 @@ import {Component, inject} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {AsyncPipe, NgIf} from "@angular/common";
 import {ViewService} from "src/app/services/view.service";
+import {TranslateModule} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, AsyncPipe, NgIf],
+  imports: [RouterLink, AsyncPipe, NgIf, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
