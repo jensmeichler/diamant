2cb18999e36810c3e8296a5e06c03af387a04418
Jens Meichler <j.meichler@rebuy.com>
2024-05-06 13:01:44 +0200

    feat: add massage page

--- src/app/pages/treatments-massage/treatments-massage.component.ts
+++ src/app/pages/treatments-massage/treatments-massage.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {InfoBoxCouponComponent} from "src/app/components/info-box-coupon/info-box-coupon.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-treatments-massage',
   standalone: true,
-  imports: [InfoBoxCouponComponent],
+  imports: [InfoBoxCouponComponent, TranslateModule],
   templateUrl: './treatments-massage.component.html',
   styleUrls: ['./treatments-massage.component.scss']
 })
