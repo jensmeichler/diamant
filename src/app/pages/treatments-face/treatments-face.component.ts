8907ddb7273ab9af12a6ef1780b8edde09216a60
Jens Meichler <j.meichler@rebuy.com>
2024-05-06 13:54:39 +0200

    feat: localization for the last pages

--- src/app/pages/treatments-face/treatments-face.component.ts
+++ src/app/pages/treatments-face/treatments-face.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {InfoBoxCouponComponent} from "src/app/components/info-box-coupon/info-box-coupon.component";
 import {ImgComponent} from "src/app/components/img/img.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-treatments-face',
   standalone: true,
-  imports: [InfoBoxCouponComponent, ImgComponent],
+  imports: [InfoBoxCouponComponent, ImgComponent, TranslateModule],
   templateUrl: './treatments-face.component.html',
   styleUrls: ['./treatments-face.component.scss']
 })
--- src/app/pages/treatments-wellness/treatments-wellness.component.ts
+++ src/app/pages/treatments-wellness/treatments-wellness.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {InfoBoxCouponComponent} from "src/app/components/info-box-coupon/info-box-coupon.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-treatments-wellness',
   standalone: true,
-  imports: [InfoBoxCouponComponent],
+  imports: [InfoBoxCouponComponent, TranslateModule],
   templateUrl: './treatments-wellness.component.html',
   styleUrls: ['./treatments-wellness.component.scss']
 })
