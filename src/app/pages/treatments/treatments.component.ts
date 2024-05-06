bf4e0c107245f66f6a1382a8179d746f3d496e80
Jens Meichler <j.meichler@rebuy.com>
2024-05-06 10:52:16 +0200

    feat(i18n): mobile button for lang change

--- src/app/pages/treatments/treatments.component.ts
+++ src/app/pages/treatments/treatments.component.ts
@@ -4,11 +4,12 @@ import {SeoService} from "src/app/services/seo.service";
 import {AsyncPipe, NgIf, NgTemplateOutlet} from "@angular/common";
 import {ViewService} from "src/app/services/view.service";
 import {ImgComponent} from "src/app/components/img/img.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-treatments',
   standalone: true,
-  imports: [RouterLink, NgIf, AsyncPipe, NgTemplateOutlet, ImgComponent],
+  imports: [RouterLink, NgIf, AsyncPipe, NgTemplateOutlet, ImgComponent, TranslateModule],
   templateUrl: './treatments.component.html',
   styleUrls: ['./treatments.component.scss']
 })
